fd0174e956c962680c0cff76afe011df2ade883e
장효진 <90826012+hyojin-dev@users.noreply.github.com>
2022-04-09 23:52:05 +0900

    UserServiceTest 작성

---
+++ src/test/java/com/example/janghj/service/UserServiceTest.java
@@ -0,0 +1,67 @@
+package com.example.janghj.service;
+
+import com.example.janghj.domain.User.User;
+import com.example.janghj.repository.UserRepository;
+import com.example.janghj.web.dto.AddressDto;
+import com.example.janghj.web.dto.UserDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@Transactional
+public class UserServiceTest {
+    @Autowired
+    UserService userService;
+    @Autowired
+    UserRepository userRepository;
+
+    UserDto userDto;
+
+    @BeforeEach
+    void beforeEach() {
+        this.userDto = new UserDto("username",
+                "password",
+                "email",
+                new AddressDto("city", "street", "zipcode"));
+    }
+
+    @Test
+    @DisplayName("유저 생성 성공")
+    void registerUser() throws Exception {
+        // given
+
+        // when
+        User user = userService.registerUser(userDto);
+
+        // then
+        Optional<User> findByOrder = userRepository.findByUsername(user.getUsername());
+        assertEquals("유저의 ID 값이 저장되어야 한다.",
+                user.getId(), findByOrder.get().getId());
+    }
+
+    @Test
+    @DisplayName("유저 삭제 성공")
+    void deleteUser() throws Exception {
+        // given
+        User user = userService.registerUser(userDto);
+
+        // when
+        userService.deleteUser(user.getId());
+
+        // then
+        Optional<User> findByOrder = userRepository.findByUsername(user.getUsername());
+        assertEquals("유저의 정보가 사라져야 한다.",
+                userRepository.findAll().size(), 0);
+    }
+}
