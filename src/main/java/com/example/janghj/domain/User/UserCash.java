575106cc053705a78c989427b80a3d6d53954b20
장효진 <90826012+hyojin-dev@users.noreply.github.com>
2022-04-13 23:05:36 +0900

    User UserCash 연관관계 수정

--- src/main/java/com/example/janghj/domain/User/UserCash.java
+++ src/main/java/com/example/janghj/domain/User/UserCash.java
@@ -2,35 +2,32 @@ package com.example.janghj.domain.User;
 
 import com.example.janghj.domain.Timestamped;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-import static javax.persistence.FetchType.LAZY;
-
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserCash extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "usercash_id")// 반드시 값을 가지도록 합니다.
+    @Column(name = "user_cash_id")// 반드시 값을 가지도록 합니다.
     private Long id;
 
     @JsonIgnore
-    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "user_id")
+    @OneToOne(mappedBy = "userCash", fetch = FetchType.LAZY)
     private User user;
 
     @Column(nullable = false)
     private int money;
 
-    public UserCash(User user) {
-        this.user = user;
-        this.money = 0;
+    public UserCash() {
     }
+
+//    public UserCash(User user) {
+//        this.user = user;
+//        this.money = 0;
+//    }
 
     public void depositUserCash(int cash) {
         this.money += cash;
