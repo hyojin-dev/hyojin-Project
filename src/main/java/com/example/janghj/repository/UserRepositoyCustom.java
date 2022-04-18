360a99a64bf7c968fe71f56ea4e5c573d4c6fe42
장효진 <90826012+hyojin-dev@users.noreply.github.com>
2022-04-18 14:02:02 +0900

    User Repositoy Page 기능 추가

--- src/main/java/com/example/janghj/repository/UserRepositoyCustom.java
+++ src/main/java/com/example/janghj/repository/UserRepositoyCustom.java
@@ -1,9 +1,16 @@
 package com.example.janghj.repository;
 
 import com.example.janghj.domain.User.User;
+import com.example.janghj.repository.dto.UserOrderDto;
+import com.example.janghj.repository.dto.UserOrderSearchDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface UserRepositoryCustom {
     List<User> findAllUser();
+    UserOrderDto findOneUserOrder(UserOrderSearchDto userOrderSearchDto);
+    List<UserOrderDto> findAllUserOrders(UserOrderSearchDto userOrderSearchDto);
+    Page<UserOrderDto> findPageUserOrders(UserOrderSearchDto userOrderSearchDto, Pageable pageable);
 }
