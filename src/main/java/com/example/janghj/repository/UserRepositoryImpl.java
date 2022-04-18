4e83df1af020ab6b5bc316d5e248e557dcef6ad5
장효진 <90826012+hyojin-dev@users.noreply.github.com>
2022-04-18 14:01:15 +0900

    User Repositoy 확장성 개선

--- src/main/java/com/example/janghj/repository/UserRepositoryImpl.java
+++ src/main/java/com/example/janghj/repository/UserRepositoryImpl.java
@@ -1,19 +1,37 @@
 package com.example.janghj.repository;
 
 import com.example.janghj.domain.User.User;
+import com.example.janghj.repository.dto.QUserOrderDto;
+import com.example.janghj.repository.dto.UserOrderDto;
+import com.example.janghj.repository.dto.UserOrderSearchDto;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import static com.example.janghj.domain.QOrder.order;
 import static com.example.janghj.domain.User.QUser.user;
 
 @Repository
-public class UserRepositoryImpl implements UserRepositoyCustom {
+public class UserRepositoryImpl implements UserRepositoryCustom {
+
     private final JPAQueryFactory queryFactory;
 
     public UserRepositoryImpl(JPAQueryFactory queryFactory) {
         this.queryFactory = queryFactory;
+    }
+
+    private BooleanExpression userIdEq(Long userId) {
+        return userId != null ? user.id.eq(Long.valueOf(userId)) : null;
+    }
+
+    private BooleanExpression orderIdEq(Long orderId) {
+        return orderId != null ? order.id.eq(Long.valueOf(orderId)) : null;
     }
 
     @Override
@@ -22,4 +40,54 @@ public class UserRepositoryImpl implements UserRepositoyCustom {
                 .selectFrom(user).fetch();
     }
 
+    @Override
+    public UserOrderDto findOneUserOrder(UserOrderSearchDto userOrderSearchDto) {
+        return queryFactory
+                .select(new QUserOrderDto(
+                        user,
+                        order
+                ))
+                .from(user)
+                .join(user.order, order)
+                .where(userIdEq(userOrderSearchDto.getUserId()),
+                        orderIdEq(userOrderSearchDto.getOrderId()))
+                .fetchOne();
+    }
+
+    @Override
+    public List<UserOrderDto> findAllUserOrders(UserOrderSearchDto userOrderSearchDto) {
+        return queryFactory
+                .select(new QUserOrderDto(
+                        user,
+                        order
+                )).from(user)
+                .innerJoin(user.order, order)
+                .where(userIdEq(userOrderSearchDto.getUserId()))
+                .fetch();
+    }
+
+    @Override
+    public Page<UserOrderDto> findPageUserOrders(UserOrderSearchDto userOrderSearchDto, Pageable pageable) {
+        List<UserOrderDto> results = queryFactory
+                .select(new QUserOrderDto(
+                        user,
+                        order
+                )).from(user)
+                .innerJoin(user.order, order)
+                .where(
+                        userIdEq(userOrderSearchDto.getUserId())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Long> countQuery = queryFactory
+                .select(user.count())
+                .from(user)
+                .innerJoin(user.order, order)
+                .where(userIdEq(userOrderSearchDto.getUserId())
+                );
+
+        return PageableExecutionUtils.getPage(results, pageable, countQuery::fetchOne);
+    }
 }
