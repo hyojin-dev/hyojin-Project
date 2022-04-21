2cd423d4b4c7830af4502b0e0b236e037d0f338b
장효진 <90826012+hyojin-dev@users.noreply.github.com>
2022-04-21 23:41:12 +0900

    QueryDsl 테스트코드 초기 세팅

---
+++ src/test/java/com/example/janghj/Repository/QueryDslTest/QueryDslOrderTest.java
@@ -0,0 +1,81 @@
+package com.example.janghj.Repository.QueryDslTest;
+
+
+import com.example.janghj.config.security.UserDetailsImpl;
+import com.example.janghj.domain.Address;
+import com.example.janghj.domain.Category;
+import com.example.janghj.domain.Order;
+import com.example.janghj.domain.Product.Product;
+import com.example.janghj.domain.Product.ProductColor;
+import com.example.janghj.domain.User.User;
+import com.example.janghj.repository.OrderRepository;
+import com.example.janghj.repository.ProductRepository;
+import com.example.janghj.repository.UserRepository;
+import com.example.janghj.repository.UserRepositoryImpl;
+import com.example.janghj.service.OrderService;
+import com.example.janghj.service.ProductService;
+import com.example.janghj.service.UserService;
+import com.example.janghj.web.dto.AddressDto;
+import com.example.janghj.web.dto.OrderWebDto;
+import com.example.janghj.web.dto.ProductDto;
+import com.example.janghj.web.dto.UserDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@Transactional
+public class QueryDslOrderTest {
+    @Autowired
+    UserRepositoryImpl userRepositoryImpl;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    UserService userService;
+    @Autowired
+    ProductService productService;
+    @Autowired
+    ProductRepository productRepository;
+    @Autowired
+    OrderService orderservice;
+    @Autowired
+    OrderRepository orderRepository;
+
+    User user;
+    UserDetailsImpl userDetails;
+    UserDto userDto;
+
+    Order order;
+    OrderWebDto orderWebDto;
+
+    @BeforeEach
+    void beforeEach() {
+        this.userDto = new UserDto("username",
+                "password",
+                "email",
+                new AddressDto("city", "street", "zipcode"));
+
+        this.user = userService.registerUser(userDto);
+        this.userDetails = new UserDetailsImpl(user);
+
+        ProductDto productDto = new ProductDto("TestProduct", 1000, 1000, Category.TOP, ProductColor.RED, 130);
+        productService.registerProduct(productDto);
+
+        Product product = productRepository.findByName("TestProduct");
+
+        Map orderList = new ConcurrentHashMap<String, Integer>();
+        String productId = product.getId().toString();
+        Integer quantity = 10;
+        orderList.put(productId, quantity);
+
+        this.orderWebDto = new OrderWebDto(orderList, new Address("city", "street", "zipcode"));
+        this.order = orderservice.order(userDetails, orderWebDto);
+    }
+}
